6fe1d1a649916adae70afeeecaa5b45d8a2c8067
Nicolò Ausili <niko.ausili@gmail.com>
2023-11-24 15:54:42 +0100

    :heavy_plus_sign:  - Add FollowMeParser utilities

---
+++ utils/src/main/java/it/unicam/cs/pa/FollowMeParserUtil.java
@@ -0,0 +1,11 @@
+package it.unicam.cs.pa;
+
+public class FollowMeParserUtil {
+
+
+
+    public static String unknownCommandMessage(int line) {
+        return String.format("Unknown command at line %d", line);
+    }
+
+}
