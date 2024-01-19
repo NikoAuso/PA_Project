1450e51c3e200d8b3510731ca6fee7c305c19df8
Nicolò Ausili <niko.ausili@gmail.com>
2024-01-19 17:12:06 +0100

    :heavy_plus_sign: - Added parsers from FollowMe repository

---
+++ lib/src/main/java/it/unicam/cs/pa/parsers/FollowMeParserException.java
@@ -0,0 +1,7 @@
+package it.unicam.cs.pa.parsers;
+
+public class FollowMeParserException extends Exception {
+    public FollowMeParserException(String message) {
+        super(message);
+    }
+}
---
+++ lib/src/main/java/it/unicam/cs/pa/parsers/FollowMeParserUtil.java
@@ -0,0 +1,8 @@
+package it.unicam.cs.pa.parsers;
+
+public class FollowMeParserUtil {
+    public static String unknownCommandMessage(int line) {
+        return String.format("Unknown command at line %d", line);
+    }
+
+}
